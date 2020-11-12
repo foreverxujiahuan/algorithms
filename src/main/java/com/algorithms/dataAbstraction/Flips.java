62dd5932e41ec9837d2d7c36aece1d074ab535fe
xu.jiahuan <xu.jiahuan@darcytech.com>
2020-11-12 21:46:24 +0800

    dataAbstraction

---
+++ src/main/java/com/algorithms/dataAbstraction/Flips.java
@@ -0,0 +1,9 @@
+package com.algorithms.dataAbstraction;
+
+
+public class Flips {
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        Counter head = new Counter("heads");
+    }
+}
